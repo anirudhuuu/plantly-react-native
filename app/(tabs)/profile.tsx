5991d75f48f33a248ea4380671a4892d8d57a5a3
Anirudh Jwala <anirudhsai2@gmail.com>
2025-01-29 16:03:34 +0530

    Layout Group implemention tab navigation with onboarding flow; refactor layout structure and rename files

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,5 +1,5 @@
-import { StyleSheet, Text, View } from "react-native";
 import { theme } from "@/theme";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function ProfileScreen() {
   return (
