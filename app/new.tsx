8ba16ce5e22eb5aa01c2d53271a4661aa83ac485
Anirudh Jwala <anirudhsai2@gmail.com>
2025-01-30 13:27:48 +0530

    Implement plant management store with add, remove, and water functionalities; integrate with new plant creation screen

--- app/new.tsx
+++ app/new.tsx
@@ -1,11 +1,16 @@
 import { PlantlyButton } from "@/component/PlantlyButton";
 import { PlantlyImage } from "@/component/PlantlyImage";
+import { usePlantStore } from "@/store/plantsStore";
 import { theme } from "@/theme";
+import { useRouter } from "expo-router";
 import { useState } from "react";
 import { Alert, StyleSheet, Text, TextInput, View } from "react-native";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
 export default function NewScreen() {
+  const router = useRouter();
+  const addPlant = usePlantStore((state) => state.addPlant);
+
   const [name, setName] = useState<string>("");
   const [days, setDays] = useState<string>("");
 
@@ -28,7 +33,8 @@ export default function NewScreen() {
       );
     }
 
-    console.log("Adding plant", name, days);
+    addPlant(name, Number(days));
+    router.navigate("/");
   };
 
   return (
